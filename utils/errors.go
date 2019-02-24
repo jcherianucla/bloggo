cf5dac26349fe121a3bc9b856c4ba29f7d5ed760
jcherianucla <jcherianucla@github.com>
2019-02-23 19:42:50 -0800

    Add more scaffolding

--- utils/errors.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-import "errors"
-
-var (
-	MDMissingSections = errors.New("Missing required sections for article")
-	MDEmpty           = errors.New("Cannot process empty markdown")
-)
