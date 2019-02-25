bc1ea78aaeb01e4fadcf5725a0f2de32b7d4348a
jcherianucla <jcherianucla@github.com>
2019-02-24 17:05:11 -0800

    Fxify clients

--- clients/module.go
+++ clients/module.go
@@ -1,1 +1,13 @@
 package clients
+
+import (
+	"github.com/jcherianucla/bloggo/clients/datastore"
+	"github.com/jcherianucla/bloggo/clients/instrumenter"
+	"go.uber.org/fx"
+)
+
+// Module defines the Fxified client modules
+var Module = fx.Options(
+	datastore.Module,
+	instrumenter.Module,
+)
